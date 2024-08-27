3150624c31c670b6b3d9fc9e160a832238da5221
Vasin Kirill <vukivur@gmail.com>
2024-08-27 21:42:04 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+let keys = document.querySelectorAll('.key');
+let buttons = document.querySelectorAll('.key');
+
+keys.forEach(key => {
+    key.addEventListener('click', playNote);
+});
+
+function playNote(e) {
+    let key = e.target;
+    let note = document.getElementById(key.dataset.note);
+    key.classList.add('active');
+    note.currentTime = 0;
+    note.play();
+    note.addEventListener('ended',() => {
+    key.classList.remove('active');
+    });
+}
+
+buttons.forEach((button) => {
+    button.addEventListener('keydown', playNote2);
+  });
+
+function playNote2(e) {
+    
+}
